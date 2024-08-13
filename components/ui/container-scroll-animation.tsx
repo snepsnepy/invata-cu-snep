99d4af0b3cc82c2758aff5ddfc41f9ea5737b773
snepsnepy <snepysneptest@gmail.com>
2024-08-13 17:35:49 +0300

    Added pricing cards and fixed responsiveness.

--- components/ui/container-scroll-animation.tsx
+++ components/ui/container-scroll-animation.tsx
@@ -27,7 +27,7 @@ export const ContainerScroll = ({
   }, []);
 
   const scaleDimensions = () => {
-    return isMobile ? [0.7, 0.9] : [1.05, 1];
+    return isMobile ? [1, 1] : [1.05, 1];
   };
 
   const rotate = useTransform(scrollYProgress, [0, 1], [20, 0]);
@@ -36,7 +36,7 @@ export const ContainerScroll = ({
 
   return (
     <div
-      className="h-[60rem] md:h-[80rem] flex items-center justify-center relative p-2 md:p-20"
+      className="h-[60rem] md:h-[80rem] flex items-center justify-center relative md:p-20"
       ref={containerRef}
     >
       <div
@@ -77,19 +77,21 @@ export const Card = ({
   translate: MotionValue<number>;
   children: React.ReactNode;
 }) => {
+  const [isMobile] = React.useState(false);
+
   return (
     <motion.div
-      style={{
-        rotateX: rotate,
-        scale,
-        boxShadow:
-          "0 0 #0000004d, 0 9px 20px #0000004a, 0 37px 37px #00000042, 0 84px 50px #00000026, 0 149px 60px #0000000a, 0 233px 65px #00000003",
-      }}
-      className="max-w-5xl -mt-12 mx-auto h-[30rem] md:h-[40rem] w-full  p-2 md:p-6 shadow-2xl"
+      style={
+        !isMobile
+          ? {
+              rotateX: rotate,
+              scale,
+            }
+          : { rotateX: 0, scale: 1 }
+      }
+      className="w-full -mt-20 lg:-mt-12 mx-auto h-full lg:h-[40rem]"
     >
-      <div className=" h-full w-full  overflow-hidden rounded-2xl bg-gray-100">
-        {children}
-      </div>
+      <div className="h-full w-full overflow-hidden">{children}</div>
     </motion.div>
   );
 };
